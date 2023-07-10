4c26135f8203f79a6e8ab06890bc1b99a66dedcd
Matheus Nunes Cerutti <matheuscerutti96@gmail.com>
2023-07-10 17:55:53 -0300

    Ajustes finais 3

--- src/services/payments-service/index.ts
+++ src/services/payments-service/index.ts
@@ -42,4 +42,6 @@ async function paymentProcess(ticketId: number, userId: number, cardData: CardPa
   return payment;
 }
 
-export default { getPaymentByTicketId, paymentProcess };
+const paymentsService = { getPaymentByTicketId, paymentProcess };
+
+export default paymentsService;
